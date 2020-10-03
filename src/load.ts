b85e1e62851bc6449d843d2f32b4979e16cb64d2
Jared Henderson <jaredh159@users.noreply.github.com>
2020-10-03 11:54:55 -0400

    add load entry (#5)

--- src/load.ts
+++ src/load.ts
@@ -1,6 +1,13 @@
-import path from 'path';
+import fs from 'fs';
 import dotenv from 'dotenv';
 
-dotenv.config({
-  path: path.join(__dirname, `..`, `..`, `..`, `.env`),
-});
+const SYMLINKED_PATH = `${__dirname}/../../../.env`;
+const LOCAL_INSTALL_PATH = `${__dirname}/../../../../../../.env`;
+
+if (!process.env.CI) {
+  if (fs.existsSync(SYMLINKED_PATH)) {
+    dotenv.config({ path: SYMLINKED_PATH });
+  } else if (fs.existsSync(LOCAL_INSTALL_PATH)) {
+    dotenv.config({ path: LOCAL_INSTALL_PATH });
+  }
+}
