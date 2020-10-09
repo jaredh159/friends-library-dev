1f724e530d3bc9b8c95bb5d027bc2905194aa461
Jared Henderson <jared@netrivet.com>
2020-10-09 14:24:16 -0400

    always purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,5 @@ const lang = process.env.GATSBY_LANG === `es` ? `es` : `en`;
 
 module.exports = {
   ...getTailwindConfig(lang),
-  purge: {
-    printRejected: true,
-    enabled: process.env.ANALYZE_BUNDLE_SIZE !== `true`,
-    content: [`./src/**/*.{ts,tsx}`],
-  },
+  purge: [`./src/**/*.tsx`],
 };
