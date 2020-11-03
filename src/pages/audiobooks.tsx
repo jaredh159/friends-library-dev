9aa72c47b0d55178ee82515ea6a58900f36077a9
Jared Henderson <jared@netrivet.com>
2020-11-03 13:45:12 -0500

    fix spanish audiobooks page bug
    for some reason the spanish site only was getting incorrect query data
    for recent audiobooks, even though in dev mode the data was correct
    when querying against GraphiQL. not doing a separate query for recent
    books and rather just ordering and slicing the main exhaustive list
    of all audiobooks seems to work just as well, and without the bug

--- src/pages/audiobooks.tsx
+++ src/pages/audiobooks.tsx
@@ -19,7 +19,9 @@ type AudioBookNode = CoverData & {
   htmlShortTitle: string;
   editions: {
     audio: {
+      added: string;
       duration: string;
+      publishedDate: string;
     };
   }[];
 };
@@ -34,99 +36,96 @@ interface Props {
     audioBooks: {
       nodes: AudioBookNode[];
     };
-    recent: {
-      nodes: (AudioBookNode & {
-        editions: {
-          audio: {
-            publishedDate: string;
-          };
-        }[];
-      })[];
-    };
   };
 }
 
-const AudiobooksPage: React.FC<Props> = ({
-  data: { audioBooks, recent, headphones },
-}: Props) => (
-  <Layout>
-    <Seo
-      title={t`Audio Books`}
-      description={PAGE_META_DESCS.audiobooks[LANG].replace(
-        /%NUM_AUDIOBOOKS%/g,
-        String(audioBooks.nodes.length),
-      )}
-    />
-    <AudiobooksHero
-      bgImg={headphones.image.fluid}
-      className="p-16 pb-48 md:pb-56"
-      numBooks={audioBooks.nodes.length}
-    />
-    <div className="bg-flgray-200 py-12 xl:pb-6">
-      <Dual.H2 className="sans-wider text-center text-2xl md:text-3xl mb-12 px-10">
-        <>Recently Added Audio Books</>
-        <>Audiolibros añadidos recientemente</>
-      </Dual.H2>
-      <Stack
-        // purgeCSS: mb-16 xl:mb-0
-        space="16"
-        xl="0"
-        className="md:px-6 lg:px-24 xl:px-0 xl:flex flex-wrap justify-center"
-      >
-        {recent.nodes.map((book) => (
-          <BookTeaserCard
-            key={book.documentUrl}
-            className="xl:w-2/5 xl:mx-8 xl:mb-12"
-            {...coverPropsFromQueryData(book)}
-            htmlShortTitle={book.htmlShortTitle}
-            audioDuration={book.editions[0].audio.duration}
-            badgeText={book.editions[0].audio.publishedDate}
-            authorUrl={book.authorUrl}
-            documentUrl={book.documentUrl}
-            description={
-              book.description ||
-              `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequ`
-            }
-          />
-        ))}
-      </Stack>
-    </div>
-    <div className="pt-16">
-      <h2 className="sans-wider text-center text-3xl mb-8 px-8">{t`All Audio Books`}</h2>
-      <Dual.P className="body-text text-center text-lg mb-12 px-8 sm:px-10">
-        <>
-          Browse all {audioBooks.nodes.length} audiobooks below, or{` `}
-          <Link to={t`/audio-help`} className="subtle-link">
-            get help with listening here
-          </Link>
-          .
-        </>
-        <>
-          Explora los {audioBooks.nodes.length} audiolibros a continuación u{` `}
-          <Link to={t`/audio-help`} className="subtle-link">
-            obtén ayuda aquí para escuchar
-          </Link>
-          .
-        </>
-      </Dual.P>
-      <div className="flex flex-col md:flex-row flex-wrap items-center md:items-stretch justify-center">
-        {audioBooks.nodes.map((book, idx) => (
-          <Audiobook
-            key={book.documentUrl}
-            className="mb-12 sm:w-4/5 md:w-2/5 md:box-content md:mx-4 xl:w-1/4 xl:max-w-3xl"
-            {...coverPropsFromQueryData(book)}
-            htmlShortTitle={book.htmlShortTitle}
-            bgColor={[`blue`, `green`, `gold`][idx % 3] as 'blue' | 'green' | 'gold'}
-            duration={(book.editions[0].audio || { duration: book.documentUrl }).duration}
-            authorUrl={book.authorUrl}
-            documentUrl={book.documentUrl}
-            description={book.description}
-          />
-        ))}
+const AudiobooksPage: React.FC<Props> = ({ data: { audioBooks, headphones } }: Props) => {
+  const sortedBooks = audioBooks.nodes.sort((a, b) =>
+    a.editions[0].audio.added < b.editions[0].audio.added ? 1 : -1,
+  );
+
+  return (
+    <Layout>
+      <Seo
+        title={t`Audio Books`}
+        description={PAGE_META_DESCS.audiobooks[LANG].replace(
+          /%NUM_AUDIOBOOKS%/g,
+          String(sortedBooks.length),
+        )}
+      />
+      <AudiobooksHero
+        bgImg={headphones.image.fluid}
+        className="p-16 pb-48 md:pb-56"
+        numBooks={sortedBooks.length}
+      />
+      <div className="bg-flgray-200 py-12 xl:pb-6">
+        <Dual.H2 className="sans-wider text-center text-2xl md:text-3xl mb-12 px-10">
+          <>Recently Added Audio Books</>
+          <>Audiolibros añadidos recientemente</>
+        </Dual.H2>
+        <Stack
+          // purgeCSS: mb-16 xl:mb-0
+          space="16"
+          xl="0"
+          className="md:px-6 lg:px-24 xl:px-0 xl:flex flex-wrap justify-center"
+        >
+          {sortedBooks.slice(0, 2).map((book) => (
+            <BookTeaserCard
+              key={book.documentUrl}
+              className="xl:w-2/5 xl:mx-8 xl:mb-12"
+              {...coverPropsFromQueryData(book)}
+              htmlShortTitle={book.htmlShortTitle}
+              audioDuration={book.editions[0].audio.duration}
+              badgeText={book.editions[0].audio.publishedDate}
+              authorUrl={book.authorUrl}
+              documentUrl={book.documentUrl}
+              description={
+                book.description ||
+                `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequ`
+              }
+            />
+          ))}
+        </Stack>
       </div>
-    </div>
-  </Layout>
-);
+      <div className="pt-16">
+        <h2 className="sans-wider text-center text-3xl mb-8 px-8">{t`All Audio Books`}</h2>
+        <Dual.P className="body-text text-center text-lg mb-12 px-8 sm:px-10">
+          <>
+            Browse all {sortedBooks.length} audiobooks below, or{` `}
+            <Link to={t`/audio-help`} className="subtle-link">
+              get help with listening here
+            </Link>
+            .
+          </>
+          <>
+            Explora los {sortedBooks.length} audiolibros a continuación u{` `}
+            <Link to={t`/audio-help`} className="subtle-link">
+              obtén ayuda aquí para escuchar
+            </Link>
+            .
+          </>
+        </Dual.P>
+        <div className="flex flex-col md:flex-row flex-wrap items-center md:items-stretch justify-center">
+          {sortedBooks.map((book, idx) => (
+            <Audiobook
+              key={book.documentUrl}
+              className="mb-12 sm:w-4/5 md:w-2/5 md:box-content md:mx-4 xl:w-1/4 xl:max-w-3xl"
+              {...coverPropsFromQueryData(book)}
+              htmlShortTitle={book.htmlShortTitle}
+              bgColor={[`blue`, `green`, `gold`][idx % 3] as 'blue' | 'green' | 'gold'}
+              duration={
+                (book.editions[0].audio || { duration: book.documentUrl }).duration
+              }
+              authorUrl={book.authorUrl}
+              documentUrl={book.documentUrl}
+              description={book.description}
+            />
+          ))}
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
 export default AudiobooksPage;
 
@@ -141,24 +140,7 @@ export const query = graphql`
         description: partialDescription
         editions {
           audio {
-            duration
-          }
-        }
-      }
-    }
-    recent: allDocument(
-      filter: { hasAudio: { eq: true } }
-      sort: { fields: editions___audio___added, order: DESC }
-      limit: 2
-    ) {
-      nodes {
-        ...CoverProps
-        documentUrl: url
-        htmlShortTitle
-        authorUrl
-        description: partialDescription
-        editions {
-          audio {
+            added
             duration
             publishedDate
           }
