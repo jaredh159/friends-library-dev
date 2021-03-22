66c7064c01185643b2d99829c1dfc5fb6efb65b4
Jared Henderson <jaredh159@users.noreply.github.com>
2021-03-22 15:58:45 -0400

    need to export newly added title fns (#7)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { quotify, quotifyLine } from './quotify';
 export { splitLines, makeSplitLines, refMutate, refUnmutate } from './split';
+export { htmlShortTitle, htmlTitle, utf8ShortTitle } from './title';
 export {
   adocFragmentToHtml,
   backtickQuotesToEntities,
