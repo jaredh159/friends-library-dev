5687fb0523eb1bd5417bd4153d2775610fa0c62d
Jared Henderson <jaredh159@users.noreply.github.com>
2021-03-24 13:42:38 -0400

    re-export ParserError for convenience (#2)

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ export { default as PdfSrcResult } from './html/result/PdfSrcResult';
 export { default as EbookSrcResult } from './html/result/EbookSrcResult';
 export { default as ChapterResult } from './html/result/ChapterResult';
 
+// convenience re-export for libs that only want to evaluate, but also handle ParserError's
+export { default as ParserError } from '@friends-library/parser';
+
 const evaluate = {
   toPdfSrcHtml,
   toEbookSrcHtml,
