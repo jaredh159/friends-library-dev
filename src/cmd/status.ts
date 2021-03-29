199c2c92c6bdabd40fdf29e3d0323798742838d7
Jared Henderson <jaredh159@users.noreply.github.com>
2021-03-29 11:52:34 -0400

    status cmd supports --diff flag (#3)

--- src/cmd/status.ts
+++ src/cmd/status.ts
@@ -1,11 +1,12 @@
-import { Options } from 'yargs';
+import { CommandBuilder } from 'yargs';
 import { red, green } from 'x-chalk';
+import exec from 'x-exec';
 import chalk from 'chalk';
 import { Argv } from '../type';
 import { getRepos, getStatusGroups } from '../repos';
 import { excludable, scopeable, relPath } from './helpers';
 
-export async function handler({ exclude, scope }: Argv): Promise<void> {
+export async function handler({ exclude, scope, diff }: Argv): Promise<void> {
   const repos = await getRepos(exclude, scope);
   const { dirty } = await getStatusGroups(repos);
   if (dirty.length === 0) {
@@ -16,6 +17,7 @@ export async function handler({ exclude, scope }: Argv): Promise<void> {
   red(`🚽  Uncommitted changes found in ${dirty.length} repos:`);
   dirty.forEach((repo) => {
     console.log(`   ${chalk.grey(`↳`)} ${chalk.yellow(relPath(repo))}`);
+    diff && exec.out(`git diff`, repo);
   });
 }
 
@@ -23,7 +25,12 @@ export const command = [`status`, `s`];
 
 export const describe = `Reports the current status for all repos`;
 
-export const builder: { [key: string]: Options } = {
-  ...excludable,
-  ...scopeable,
+export const builder: CommandBuilder = function (yargs) {
+  return yargs
+    .option(`exclude`, excludable.exclude)
+    .option(`scopeable`, scopeable.scope)
+    .option(`diff`, {
+      default: false,
+      type: `boolean`,
+    });
 };
