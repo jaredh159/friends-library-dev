1b25b71496d1a04c09c8d8060b8987cee40631d3
Jared Henderson <jared@netrivet.com>
2021-03-29 11:44:07 -0400

    pdf running heads should be preprocessed for asciidoc-isms

--- src/pdf-shared.ts
+++ src/pdf-shared.ts
@@ -1,13 +1,15 @@
 import { Xml, DocPrecursor } from '@friends-library/types';
-
-export function lineSvgMarkup(): Xml {
-  return `<svg height="1px" width="88px" version="1.1" xmlns="http://www.w3.org/2000/svg"><line x1="0" y1="0" x2="88" y2="0" style="stroke:rgb(0,0,0);stroke-width:1" /></svg>`;
-}
+import { utf8ShortTitle } from '@friends-library/adoc-utils';
 
 export function runningHead(
   dpc: Pick<DocPrecursor, 'meta' | 'config'>,
   numChapters: number,
 ): string {
   const { meta, config } = dpc;
-  return numChapters === 1 ? meta.author.name : config.shortTitle || meta.title;
+  const title = numChapters === 1 ? meta.author.name : config.shortTitle || meta.title;
+  return utf8ShortTitle(title);
+}
+
+export function lineSvgMarkup(): Xml {
+  return `<svg height="1px" width="88px" version="1.1" xmlns="http://www.w3.org/2000/svg"><line x1="0" y1="0" x2="88" y2="0" style="stroke:rgb(0,0,0);stroke-width:1" /></svg>`;
 }
