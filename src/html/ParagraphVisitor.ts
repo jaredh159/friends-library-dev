353695186a05ed2841bcd9bfdfb3d09b16fac6eb
Jared Henderson <jaredh159@users.noreply.github.com>
2021-03-30 11:33:44 -0400

    fix bug with small breaks after .numbered paragraphs (#5)

--- src/html/ParagraphVisitor.ts
+++ src/html/ParagraphVisitor.ts
@@ -2,10 +2,16 @@ import { Visitable, AstNode, VisitData, NODE as n } from '@friends-library/parse
 import { utils as u, chapterMarkup as c } from '../utils';
 
 export default class ParagraphVisitor implements Visitable {
+  protected subGroupIsOpen = false;
+
   enter({ node }: VisitData): void {
     const el = isChapterSubtitleBlurb(node) ? `h3` : `p`;
     if (opensNumberedSubgroup(node)) {
       c.push(`<div${u.classAttr(node)}>`);
+      // this seems like a smell... i think it's telling me
+      // that the parser should be creating a wrapping node
+      // so that the evaluator doesn't have to be this smart or stateful...
+      this.subGroupIsOpen = true;
     }
     c.push(`<${el}${classAttr(el, node)}>`);
   }
@@ -13,23 +19,27 @@ export default class ParagraphVisitor implements Visitable {
   exit({ node }: VisitData): void {
     const el = isChapterSubtitleBlurb(node) ? `h3` : `p`;
     c.push(`</${el}>`);
-    if (closesNumberedSubgroup(node)) {
+    if (this.closesNumberedSubgroup(node)) {
       c.push(`</div>`);
+      this.subGroupIsOpen = false;
     }
+  }
+
+  closesNumberedSubgroup(node: AstNode): boolean {
+    if (!this.subGroupIsOpen) {
+      return false;
+    }
+    return !!(
+      node.parent.hasClass(`numbered-group`) &&
+      (node.isLastChild() ||
+        node.nextSibling()?.hasClass(`numbered`) ||
+        node.nextSibling()?.type !== n.PARAGRAPH)
+    );
   }
 }
 
 function opensNumberedSubgroup(node: AstNode): boolean {
   return node.hasClass(`numbered`);
-}
-
-function closesNumberedSubgroup(node: AstNode): boolean {
-  return !!(
-    node.parent.hasClass(`numbered-group`) &&
-    (node.isLastChild() ||
-      node.nextSibling()?.hasClass(`numbered`) ||
-      node.nextSibling()?.type !== n.PARAGRAPH)
-  );
 }
 
 function isChapterSubtitleBlurb(node: AstNode): boolean {
--- src/html/__tests__/adoc-test-case.ts
+++ src/html/__tests__/adoc-test-case.ts
@@ -15,66 +15,76 @@ export function getTestCases<T>(
   const tests = file.split(/## /g);
   consume(tests, `comment`);
 
-  const cases: TestCase[] = tests.map((block) => {
-    const lines = block.split(`\n`);
-    const title = lines.shift() ?? ``;
+  return (
+    tests
+      .map((block) => {
+        const lines = block.split(`\n`);
+        const title = lines.shift() ?? ``;
 
-    consume(lines, `comment`);
-    consume(lines, `empty`);
+        consume(lines, `comment`);
+        consume(lines, `empty`);
 
-    const files: Array<{ adoc: string; filename: string }> = [];
-    let chapterHeadingOmitted = false;
+        const files: Array<{ adoc: string; filename: string }> = [];
+        let chapterHeadingOmitted = false;
 
-    // create a {adoc: string, filename: string} obj for each "file"
-    while (lines[0] === `****`) {
-      // buffer up asciidoc into `adocLines`
-      const adocLines: string[] = [];
-      consume(lines, `adoc-delim`);
-      while (lines[0] !== `****`) {
-        adocLines.push(lines.shift() ?? ``);
-      }
+        // create a {adoc: string, filename: string} obj for each "file"
+        while (lines[0] === `****`) {
+          // buffer up asciidoc into `adocLines`
+          const adocLines: string[] = [];
+          consume(lines, `adoc-delim`);
+          while (lines[0] !== `****`) {
+            adocLines.push(lines.shift() ?? ``);
+          }
 
-      let adoc = adocLines.join(`\n`);
-      if (!adoc.match(/^== /m)) {
-        chapterHeadingOmitted = true;
-        adoc = `== Preface\n\n${adoc}`;
-      }
+          let adoc = adocLines.join(`\n`);
+          if (!adoc.match(/^== /m)) {
+            chapterHeadingOmitted = true;
+            adoc = `== Preface\n\n${adoc}`;
+          }
 
-      files.push({ adoc: adoc, filename: `0${files.length + 1}-journal.adoc` });
-      consume(lines, `adoc-delim`);
-      consume(lines, `empty`);
-    }
+          files.push({ adoc: adoc, filename: `0${files.length + 1}-journal.adoc` });
+          consume(lines, `adoc-delim`);
+          consume(lines, `empty`);
+        }
 
-    // files array is complete, parse expected outcome html
-    consume(lines, `text-delim`);
-    const expectedLines: string[] = [];
-    while (lines[0] !== `++++`) {
-      expectedLines.push(lines.shift() ?? ``);
-    }
+        // files array is complete, parse expected outcome html
+        consume(lines, `text-delim`);
+        const expectedLines: string[] = [];
+        while (lines[0] !== `++++`) {
+          expectedLines.push(lines.shift() ?? ``);
+        }
 
-    let expectedOutput = expectedLines.join(`\n`);
-    if (chapterHeadingOmitted && supplyMissingChapterHeadings) {
-      expectedOutput = [
-        `<div id="chapter-1" class="chapter chapter-1 chapter--no-signed-section">`,
-        `<!-- preface chapter heading markup here -->`,
-        expectedOutput,
-        `</div>`,
-      ].join(`\n`);
-    }
+        let expectedOutput = expectedLines.join(`\n`);
+        if (chapterHeadingOmitted && supplyMissingChapterHeadings) {
+          expectedOutput = [
+            `<div id="chapter-1" class="chapter chapter-1 chapter--no-signed-section">`,
+            `<!-- preface chapter heading markup here -->`,
+            expectedOutput,
+            `</div>`,
+          ].join(`\n`);
+        }
 
-    expectedOutput = expectedOutput.replace(
-      `<!-- preface chapter heading markup here -->`,
-      `<header class="chapter-heading" data-short="Preface"><h2>Preface</h2></header>`,
-    );
+        expectedOutput = expectedOutput.replace(
+          `<!-- preface chapter heading markup here -->`,
+          `<header class="chapter-heading" data-short="Preface"><h2>Preface</h2></header>`,
+        );
 
-    const dpc = getDpc(...files.map((f) => f.adoc));
-    const src = evaluator(dpc);
-    const actualOutput = selector(src);
-    return [title, prettify(expectedOutput), prettify(actualOutput)];
-  });
-
-  const isolated = cases.filter(([title]) => title.includes(`(only)`));
-  return isolated.length ? ([isolated[0]] as TestCase[]) : cases;
+        const dpc = getDpc(...files.map((f) => f.adoc));
+        return { title, expected: prettify(expectedOutput), dpc };
+      })
+      // isolate (only tests)
+      .reduce((acc, testCase) => {
+        if (testCase.title.includes(`(only)`)) {
+          return [testCase];
+        }
+        return [...acc, testCase];
+      }, [] as Array<{ title: string; expected: string; dpc: DocPrecursor }>)
+      .map((testCase) => [
+        testCase.title,
+        testCase.expected,
+        prettify(selector(evaluator(testCase.dpc))),
+      ])
+  );
 }
 
 function consume(
