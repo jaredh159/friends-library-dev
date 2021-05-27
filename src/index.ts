fdd8fdd6dd769cb3ca1ae9655b588d2b78bc5bda
Jared Henderson <jared@netrivet.com>
2021-05-27 13:42:51 -0400

    add square image sizes type, array

--- src/index.ts
+++ src/index.ts
@@ -80,6 +80,20 @@ export const SHIPPING_LEVELS = [
 ] as const;
 export type ShippingLevel = typeof SHIPPING_LEVELS[number];
 
+export const SQUARE_COVER_IMAGE_SIZES = [
+  90,
+  180,
+  270,
+  300,
+  450,
+  600,
+  750,
+  900,
+  1150,
+  1400,
+] as const;
+export type SquareCoverImageSize = typeof SQUARE_COVER_IMAGE_SIZES[number];
+
 export const AUDIO_QUALITIES = [`HQ`, `LQ`] as const;
 export type AudioQuality = typeof AUDIO_QUALITIES[number];
 
