e010bb47ae6f2462e29d73649cb919c0a4171c51
Jared Henderson <jaredh159@users.noreply.github.com>
2021-06-08 11:15:22 -0400

    npm dev dep to prevent jest breaking changes (#12)

--- src/image-sizes.ts
+++ src/image-sizes.ts
@@ -11,16 +11,7 @@ export const LARGEST_THREE_D_COVER_IMAGE_WIDTH: ThreeDCoverImageWidth =
 export const THREE_D_COVER_IMAGE_ASPECT_RATIO = LARGEST_THREE_D_COVER_IMAGE_WIDTH / 1640;
 
 export const SQUARE_COVER_IMAGE_SIZES = [
-  90,
-  180,
-  270,
-  300,
-  450,
-  600,
-  750,
-  900,
-  1150,
-  1400,
+  90, 180, 270, 300, 450, 600, 750, 900, 1150, 1400,
 ] as const;
 
 export type SquareCoverImageSize = typeof SQUARE_COVER_IMAGE_SIZES[number];
