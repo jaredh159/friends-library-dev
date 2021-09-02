c5eb96c2a747e1d6c566a83a2bcd05d530d20be7
Jared Henderson <jaredh159@users.noreply.github.com>
2021-09-02 12:30:09 -0400

    update deps + build with new db functions (#95)

--- src/components/checkout/services/CheckoutApi.ts
+++ src/components/checkout/services/CheckoutApi.ts
@@ -48,7 +48,7 @@ export default class CheckoutApi {
         statusCode: 500,
         data: {
           msg: Err.ERROR_CONFIRMING_STRIPE_CARD_PAYMENT,
-          details: error.message,
+          details: String(error),
         },
       };
     }
