3ae63bfbfc579e1f3dd127da188c6b179a1379a8
Jared Henderson <jared@netrivet.com>
2021-09-07 15:31:29 -0400

    tweak signin screen

--- src/SignIn.tsx
+++ src/SignIn.tsx
@@ -10,7 +10,7 @@ const SignIn: React.FC<Props> = ({ setToken }) => {
   const validInput = input.length === 36;
   return (
     <div className="min-h-screen bg-white flex">
-      <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+      <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20">
         <div className="mx-auto w-full max-w-sm lg:w-96">
           <div className="mt-8">
             <div className="mt-6">
@@ -26,7 +26,7 @@ const SignIn: React.FC<Props> = ({ setToken }) => {
                 <div>
                   <label
                     htmlFor="email"
-                    className="block text-sm font-light text-gray-600 antialiased"
+                    className="block text-md font-light text-gray-600"
                   >
                     Enter Authorization Token:
                   </label>
@@ -38,7 +38,7 @@ const SignIn: React.FC<Props> = ({ setToken }) => {
                       spellCheck={false}
                       value={input}
                       onChange={(e) => setInput(e.target.value)}
-                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-300 focus:outline-none focus:ring-flprimary-500 focus:border-flprimary-500 sm:text-sm"
+                      className="font-mono appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-200 focus:outline-none focus:ring-flprimary-500 focus:border-flprimary-500 sm:text-md"
                     />
                   </div>
                 </div>
@@ -47,9 +47,8 @@ const SignIn: React.FC<Props> = ({ setToken }) => {
                     type="submit"
                     disabled={!validInput}
                     className={cx(
-                      !validInput && `opacity-75 bg-gray-400 cursor-not-allowed`,
-                      validInput && `bg-flprimary`,
-                      `font-sans antialiased w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm text-white hover:bg-flprimary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-flprimary-500`,
+                      !validInput && `opacity-75 cursor-not-allowed`,
+                      `font-sans bg-flprimary antialiased w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-md text-white hover:bg-flprimary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-flprimary-500`,
                     )}
                   >
                     Authorize
