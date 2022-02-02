bdcab52d191e25e7589cd82303f652984dcf8be7
Jared Henderson <jared@netrivet.com>
2022-02-02 10:05:56 -0500

    change format of result helpers

--- src/lib/result.ts
+++ src/lib/result.ts
@@ -1,17 +1,19 @@
 import { ApolloError } from '@apollo/client';
 
-class ResultHelpers {
-  apolloError(apolloError: ApolloError | undefined): Result<never, string> {
-    return this.error(`${apolloError ?? `missing data`}`);
-  }
-
-  error<T>(error: T): Result<never, T> {
-    return { success: false, error: error };
-  }
-
-  success<T>(value: T): Result<T, never> {
-    return { success: true, value: value };
-  }
+function apolloError(apolloError: ApolloError | undefined): Result<never, string> {
+  return error(`${apolloError ?? `missing data`}`);
 }
 
-export default new ResultHelpers();
+function error<T>(error: T): Result<never, T> {
+  return { success: false, error: error };
+}
+
+function success<T>(value: T): Result<T, never> {
+  return { success: true, value: value };
+}
+
+export default {
+  error,
+  apolloError,
+  success,
+};
