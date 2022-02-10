6483a22730ab066ef64cf9ea7fd7d11a6d18b092
Jared Henderson <jared@netrivet.com>
2022-02-10 12:01:30 -0500

    use new beta db lib

--- src/client.ts
+++ src/client.ts
@@ -1,34 +1,10 @@
-import { ApolloClient, InMemoryCache, createHttpLink, from, gql } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import { gql } from '@apollo/client';
+import { getClient } from '@friends-library/db';
 
-const httpLink = createHttpLink({
-  uri: import.meta.env.SNOWPACK_PUBLIC_FLP_GRAPHQL_API_ENDPOINT,
+export default getClient({
+  env: `dev`,
+  token: localStorage.getItem(`token`),
 });
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem(`token`);
-  return {
-    headers: {
-      ...headers,
-      ...(token ? { Authorization: `Bearer ${token}` } : {}),
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: from([authLink, httpLink]),
-  cache: new InMemoryCache(),
-});
-
-export default client;
-
-declare global {
-  interface ImportMeta {
-    env: {
-      SNOWPACK_PUBLIC_FLP_GRAPHQL_API_ENDPOINT: string;
-    };
-  }
-}
 
 export const SELECTABLE_DOCUMENTS_FIELDS = gql`
   fragment SelectableDocumentsFields on Document {
