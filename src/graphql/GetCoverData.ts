36ce7ac55f4df5c4195cca54dc82f06e21750303
Jared Henderson <jared@netrivet.com>
2022-02-16 13:28:52 -0500

    update cover cmd

---
+++ src/graphql/GetCoverData.ts
@@ -0,0 +1,30 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+import { EditionType, PrintSizeVariant } from './globalTypes';
+
+// ====================================================
+// GraphQL query operation: GetCoverData
+// ====================================================
+
+export interface GetCoverData_edition_impression {
+  __typename: 'EditionImpression';
+  paperbackVolumes: number[];
+  paperbackSize: PrintSizeVariant;
+}
+
+export interface GetCoverData_edition {
+  __typename: 'Edition';
+  type: EditionType;
+  impression: GetCoverData_edition_impression | null;
+}
+
+export interface GetCoverData {
+  edition: GetCoverData_edition;
+}
+
+export interface GetCoverDataVariables {
+  id: UUID;
+}
