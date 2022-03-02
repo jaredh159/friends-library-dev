fe57a6dce23a219c6d51c476edaedcac84458498
Jared Henderson <jared@netrivet.com>
2022-03-02 10:04:50 -0500

    handle comma-separated multiple ip addresses breaking location api reqs

--- Sources/App/Extensions/Request.swift
+++ Sources/App/Extensions/Request.swift
@@ -2,8 +2,10 @@ import Vapor
 
 extension Request {
   var ipAddress: String? {
-    headers.first(name: .xForwardedFor)
+    let ip = headers.first(name: .xForwardedFor)
       ?? headers.first(name: "X-Real-IP")
       ?? remoteAddress?.ipAddress
+    // sometimes we get multiple ip addresses, like `1.2.3.4, 1.2.3.5`
+    return ip?.split(separator: ",").first.map { String($0) }
   }
 }
