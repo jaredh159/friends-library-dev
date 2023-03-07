32b29d1063239a9cd3705ffbe4bc5337d096e3bd
Jared Henderson <jaredh159@users.noreply.github.com>
2023-03-07 16:08:53 -0500

    cli: tweak poster server based on last round of youtube uploads

--- apps/cli/src/cmd/video/poster-server.ts
+++ apps/cli/src/cmd/video/poster-server.ts
@@ -13,6 +13,7 @@ export async function screenshot(
   port: number,
 ): Promise<[ScreenshotTaker, BrowserCloser]> {
   const { CHROMIUM_PATH } = env.require(`CHROMIUM_PATH`);
+  // if you get error launching, try `unset PUPPETEER_PRODUCT`
   const browser = await puppeteer.launch({ executablePath: CHROMIUM_PATH });
   const page = await browser.newPage();
   await page.setViewport({ width: 1920, height: 1080 });
@@ -20,7 +21,10 @@ export async function screenshot(
   return [
     async (path: string): Promise<Buffer> => {
       await page.goto(`http://localhost:${port}/${path}`);
-      return page.screenshot({ encoding: `binary` }) as Promise<Buffer>;
+      return page.screenshot({
+        encoding: `binary`,
+        clip: { x: 0, y: 0, width: 1920, height: 1080 },
+      }) as Promise<Buffer>;
     },
     async () => await browser.close(),
   ];
