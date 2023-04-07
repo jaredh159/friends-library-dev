a5a1fbf9a2a49f2c0a6126883a586fcc3a05645c
Jared Henderson <jaredh159@users.noreply.github.com>
2023-04-07 15:40:12 -0400

    next-evans: poc prisma integration

--- apps/next-evans/pages/index.tsx
+++ apps/next-evans/pages/index.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
+import { PrismaClient } from '@prisma/client';
+import type { friends } from '@prisma/client';
 import type { GetStaticProps } from 'next';
 
 export const getStaticProps: GetStaticProps = async () => {
+  const prisma = new PrismaClient();
+  const friends = await prisma.friends.findMany();
   return {
-    props: {},
+    props: {
+      friends: friends.map((friend) => ({
+        name: friend.name,
+        gender: friend.gender,
+      })),
+    },
   };
 };
 
-const Home: React.FC = () => (
-  <h1 className="bg-flprimary text-white">
-    Home, lang is <code className="text-red-700">{process.env.NEXT_PUBLIC_LANG}</code>
-  </h1>
+const Home: React.FC<{ friends: friends[] }> = ({ friends }) => (
+  <div>
+    <h1 className="bg-flprimary text-white">
+      Home, lang is <code className="text-red-700">{process.env.NEXT_PUBLIC_LANG}</code>
+    </h1>
+    <ul>
+      {friends.map((friend) => (
+        <li key={friend.id}>
+          {friend.name} is {friend.gender}
+        </li>
+      ))}
+    </ul>
+  </div>
 );
 
 export default Home;
