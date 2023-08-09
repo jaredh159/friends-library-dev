11fcb8be2d2218454732294e4dad01234f5f45de
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2023-08-09 08:38:11 -0400

    next-evans: fixed ul styling and removed paths from mdx frontmatter

--- apps/next-evans/pages/_app.tsx
+++ apps/next-evans/pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
 import '@/styles/cover.css';
 
-const App: React.FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
+const App: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <Component {...pageProps} />
+);
 
 export default App;
