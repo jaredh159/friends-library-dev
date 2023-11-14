08c9ee2d961f453024b0c5847ac69ab9505f49e6
Jared Henderson <jaredh159@users.noreply.github.com>
2023-11-14 09:53:15 -0500

    db: remove db package, in favor of pairql

--- libs-ts/db/src/index.ts
+++
@@ -1,7 +0,0 @@
-export { gql } from '@apollo/client';
-export { getClient } from './Client';
-export type { ClientConfig, ClientType } from './Client';
-
-export function writable<T>(apolloReadOnlyObject: T): T {
-  return JSON.parse(JSON.stringify(apolloReadOnlyObject));
-}
