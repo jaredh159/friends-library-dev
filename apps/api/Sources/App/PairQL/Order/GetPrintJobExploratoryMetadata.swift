1ad19c16c1d35ce5dbdb7756334c8f07d9ff0438
Jared Henderson <jaredh159@users.noreply.github.com>
2023-11-16 12:29:24 -0500

    api: throw explicit pql error for no shipping

--- apps/api/Sources/App/PairQL/Order/GetPrintJobExploratoryMetadata.swift
+++ apps/api/Sources/App/PairQL/Order/GetPrintJobExploratoryMetadata.swift
@@ -16,19 +16,29 @@ struct GetPrintJobExploratoryMetadata: Pair {
 
 extension GetPrintJobExploratoryMetadata: Resolver {
   static func resolve(with input: Input, in context: Context) async throws -> Output {
-    try await PrintJobs.getExploratoryMetadata(
-      for: input.items,
-      // codable decode skips initializer normalizing `state` to abbrev
-      shippedTo: ShippingAddress(
-        name: input.address.name,
-        street: input.address.street,
-        street2: input.address.street2,
-        city: input.address.city,
-        state: input.address.state,
-        zip: input.address.zip,
-        country: input.address.country
+    do {
+      return try await PrintJobs.getExploratoryMetadata(
+        for: input.items,
+        // codable decode skips initializer normalizing `state` to abbrev
+        shippedTo: ShippingAddress(
+          name: input.address.name,
+          street: input.address.street,
+          street2: input.address.street2,
+          city: input.address.city,
+          state: input.address.state,
+          zip: input.address.zip,
+          country: input.address.country
+        )
       )
-    )
+    } catch PrintJobs.Error.noExploratoryMetadataRetrieved {
+      throw context.error(
+        id: "53b04a22",
+        type: .notFound,
+        detail: "Error.noExploratoryMetadataRetrieved: shipping not possible"
+      )
+    } catch {
+      throw error
+    }
   }
 }
 
